054cdd69ce3e2b571e679f55d7e4e999bf1a0097
Guillaume AUGEY - SCC <17220155+guyomog78@users.noreply.github.com>
2023-08-10 17:10:19 +0200

    Take care of multiligne secrets (like SSH Keys)

--- python-script.py
+++ python-script.py
@@ -1,6 +1,11 @@
 from awx.main.utils import decrypt_field
 import os
+
 cred = Credential.objects.get(pk=os.environ['CRED_ID'])
 for key in cred.inputs:
-  if cred.inputs[key].startswith('$encrypted$'):
-    print ("{}: {}".format(key, decrypt_field(cred, key)))
+    if cred.inputs[key].startswith('$encrypted$'):
+        decrypted_value = decrypt_field(cred, key)
+        if "\n" in decrypted_value:
+            # Format multi-line strings for valid YAML output
+            decrypted_value = "|\n" + "\n".join(["  " + line for line in decrypted_value.splitlines()])
+        print("{}: {}".format(key, decrypted_value))
